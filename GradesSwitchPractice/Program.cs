db68ccb198d7a78c6a15ada54c9372eb59543fa0
dhsaifee <dhsaifee@cba.ua.edu>
2019-09-18 00:14:26 -0500

    Add project files.

---
+++ GradesSwitchPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GradesSwitchPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a letter grade: ");
+            string grade = Console.ReadLine();
+
+            switch (grade)
+            {
+                case "A":
+                    Console.WriteLine("Awesome!");
+                    break;
+                case "B":
+                    Console.WriteLine("Good job");
+                    break;
+                default:
+                    Console.WriteLine("keep trying");
+                    break;
+            }
+        }
+    }
+}
